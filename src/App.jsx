2b4b2d88f55498e1e22969aa23087eb1c6845807
Kaustubh Hiwanj <kaustubhhiwanj@Kaustubhs-MacBook-Pro.local>
2025-04-10 01:18:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Skills from "./components/skills/skills.jsx"
 import Projects from './components/Projects/Projects.jsx';
 import Contact from "./components/Contact/Contact.jsx"
 import AboutMe from './components/About_me/About.jsx';
-import Navbar from './components/Navbar/navbar.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
 
